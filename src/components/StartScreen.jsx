5f5c912b2defbf1c5c241181143fed3d28062874
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-07-15 22:30:01 -0300

    fix: select de dificuldade e msg pontuação/parabéns

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,11 +1,57 @@
-import "./StartScreen.css";
+import { useState, useEffect } from "react";
+import "../components/StartScreen.css";
 
-const StartScreen = ({ startGame }) => {
+const StartScreen = ({ startGame, showCongratulations, score }) => {
+  const [difficulty, setDifficulty] = useState("medium");
+  const [hasPlayedOnce, setHasPlayedOnce] = useState(false); // Estado para controlar se o jogo já foi jogado pelo menos uma vez
+
+  const handleChange = (e) => {
+    setDifficulty(e.target.value);
+  };
+
+  const handleStartGame = () => {
+    startGame(difficulty);
+    setHasPlayedOnce(true); // Define que o jogo foi jogado pelo menos uma vez ao iniciar o jogo
+  };
+
+  useEffect(() => {
+    // Reinicia o estado de showCongratulations quando o componente é montado novamente
+    if (hasPlayedOnce) {
+      setHasPlayedOnce(true);
+    }
+  }, [hasPlayedOnce]);
+
+
   return (
-    <div className="start">
-      <h1>Secret Word</h1>
-      <p>Clique no botão abaixo para começar a jogar</p>
-      <button onClick={startGame}>Começar jogo</button>
+    <div>
+      <h1>Jogo Palavra Secreta</h1>
+      <label>
+        Escolha a dificuldade:
+        <select value={difficulty} onChange={handleChange}>
+          <option value="easy">Fácil</option>
+          <option value="medium">Médio</option>
+          <option value="hard">Difícil</option>
+          <option value="very_hard">Muito Difícil</option>
+        </select>
+      </label>
+      <button onClick={handleStartGame}>Iniciar o Jogo</button>
+      {score == 150 && (
+        <p style={{ color: "yellow", marginTop: "10px" }}>
+          Sua pontuação inicial/atual: {score}
+        </p>
+      )}
+      {score != 150 && (
+        <p style={{ color: "yellow", marginTop: "10px" }}>
+          Sua pontuação atual: {score}
+        </p>
+      )}
+
+      {score > 150 || score < 150 && score != 0 && (
+        <p style={{ color: "green", marginTop: "10px" }}>
+          Parabéns, você acertou!
+        </p>
+      )}
+
     </div>
   );
 };
